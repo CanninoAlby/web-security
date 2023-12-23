437ba57067519e46df652f3c26216ad6b718457e
CanninoAlby <canninoalby@student.ub.ac.id>
2023-12-23 08:07:19 +0700

    deleted:    public/img/1699469734704.png 	deleted:    public/img/1699470347189.png 	deleted:    public/img/1700010620006.jpeg 	deleted:    public/img/1700380819878.jpg 	deleted:    public/img/1700381042914.jpg 	deleted:    public/img/1701331813486.jpg 	deleted:    public/img/1701332292464.jpg 	deleted:    public/img/1701332366911.jpg 	deleted:    public/img/1702855437095.png 	modified:   src/routers/GalleryRoutes.ts

--- src/routers/GalleryRoutes.ts
+++ src/routers/GalleryRoutes.ts
@@ -8,9 +8,9 @@ import Upload from "../middlewares/UploadMiddleware";
 class GalleryRoutes extends BaseRoutes{
 
     public routes(): void{
-        this.router.get("/",auth, GalleryController.index);
+        this.router.get("/", GalleryController.index);
         this.router.post("/",auth, Upload,GalleryController.create);
-        this.router.get("/:id",auth, GalleryController.show); 
+        this.router.get("/:id", GalleryController.show); 
         this.router.put("/:id",auth,Upload, GalleryController.update);
         this.router.delete("/:id",auth, GalleryController.delete);
     }
